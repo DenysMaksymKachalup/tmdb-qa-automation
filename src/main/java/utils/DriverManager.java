c11411495ca00c11b4ebea6b4885e067dc461eda
Denys-Maksym Kachalup <denys.k82@gmail.com>
2026-05-02 10:58:28 +0300

    added lang setting

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -2,8 +2,11 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -29,11 +32,25 @@ public class DriverManager {
     }
 
     private static WebDriver getDriverFromProperties() {
-        return switch (System.getProperty("browser")) {
-            case "firefox" -> new FirefoxDriver();
-            case "chrome" -> new ChromeDriver();
-            case "edge" -> new EdgeDriver();
-            default -> throw new UnsupportedOperationException("Unsupported browser: " + System.getProperty("browser"));
+        return switch (ConfigReader.getProperty("browser")) {
+            case "chrome" -> {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--lang=en");
+                yield new ChromeDriver(options);
+            }
+
+            case "firefox" -> {
+                FirefoxOptions options = new FirefoxOptions();
+                options.addPreference("intl.accept_languages", "en,en-US");
+                yield new FirefoxDriver(options);
+            }
+
+            case "edge" -> {
+                EdgeOptions options = new EdgeOptions();
+                options.addArguments("--lang=en");
+                yield new EdgeDriver(options);
+            }
+            default -> throw new UnsupportedOperationException("Unsupported browser: " + ConfigReader.getProperty("browser"));
         };
     }
 
